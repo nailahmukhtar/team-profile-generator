ae335b9a23fb536ac867439d27b7c0c0c9fa706f
Nai <nailahmukhtar@Nailahs-Air.home>
2023-03-04 21:25:50 +0000

    updated methods for all subclasses

--- starter/lib/Engineer.js
+++ starter/lib/Engineer.js
@@ -1,4 +1,3 @@
-// TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
 const Employee = require("./Employee");
 
 class Engineer extends Employee {
@@ -8,7 +7,7 @@ class Engineer extends Employee {
     }
 
     getGithub() {
-
+        return this.github;
     }
 
     getRole() {
--- starter/lib/Manager.js
+++ starter/lib/Manager.js
@@ -1,29 +1,18 @@
-// TODO: Write code to define and export the Manager class. HINT: This class should inherit from Employee.
 const Employee = require("./Employee");
 
 class Manager extends Employee {
     constructor(name, id, email, officeNumber) {
-        
         super(name, id, email);
-
         this.officeNumber = officeNumber;
-
-
     }
 
     getRole() {
         return "Manager";
     }
 
-    getOfficeNumber(value) {
-        this.officeNumber = value;
+    getOfficeNumber() {
+        return this.officeNumber;
     }
 }
 
 module.exports = Manager;  
-
-// const manager = new Manager("James", 3, "test@test.com", 145);
-// console.log(manager.getRole());
-
-// manager.setofficeNumber(136);
-// console.log(manager.officeNumber);
